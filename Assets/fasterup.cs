860a0985d98ccdf44c39c39e1c97e8673e60ef32
Tech <atech17@student.aau.dk>
2019-10-04 13:20:31 +0200

    Uploaded the ball cues .. 3 moving balls one with light one with speed

---
+++ Assets/fasterup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fasterup : MonoBehaviour
+{
+    public int maxuSpeed;
+
+    private Vector3 startuPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        maxuSpeed = 30;
+
+        startuPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveuVertical();
+    }
+
+    void MoveuVertical()
+    {
+        transform.position = new Vector3(transform.position.x, startuPosition.y + (Mathf.Sin(Time.time * maxuSpeed) * 50), transform.position.z);
+
+        if (transform.position.y > 5.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.y < -5.0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
